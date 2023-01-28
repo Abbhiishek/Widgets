66d00bb84b904268bfcdf9c0d92514d8b5bb5bcc
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-28 23:32:53 +0530

    usercard github

---
+++ components/userProfile.tsx
@@ -0,0 +1,67 @@
+
+
+export default function UserProfile({ login,
+    avatar_url,
+    name,
+    bio,
+    location,
+    blog,
+    twitter_username,
+    public_repos,
+    public_gists,
+    email,
+    followers,
+    following,
+    created_at, Theme, orgLength }: any) {
+    return (
+        `
+        <svg width="642" height="274" viewBox="0 0 642 274" fill="${Theme.bg_color || "none"}" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <title id="titleId">${name}</title>
+
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Cormorant:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;display=swap');
+
+        text {
+            font-family: 'Cormorant', monospace -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        }
+        .profile_image{
+            clip-path: circle(100px at center);
+        }
+
+        ;
+    </style>
+
+    
+    <rect x="0" y="0" width="642" height="274" rx="22" fill="${Theme.bg_color || "#fff"}" />
+    <circle cx="507" cy="132" r="100" fill="#f90000" />
+    <image href="${avatar_url}" x="398" y="26" height="216" width="220" />
+    <text font-size="32" textLength="0" x="38" y="61.2" fill="${Theme.title_color || "#000000"}">
+    ${name}
+    </text>
+    <text font-size="14" textLength="0" x="38" y="85.2" fill="${Theme.text_color || "#000000"}">
+    ${bio}
+    </text>
+    <text font-size="12" textLength="0" x="38" y="109.2" fill="${Theme.text_color || "#000000"}">
+    ${twitter_username}
+    </text>
+    <text font-size="10" textLength="0" x="181" y="107.2" fill="${Theme.text_color || "#000000"}">
+    ${email ? email : ""}
+    </text>
+    <text font-size="18" x="38" y="139.5" fill="${Theme.text_color || "#000000"}">
+    ${followers} Followers | ${following} Following
+    </text>
+
+    <text font-size="12" x="38" y="177.9" fill="${Theme.text_color || "#000000"}">Joined at :  ${(new Date(created_at)).toDateString()}</text>
+    <text font-size="18" x="38" y="233.5" fill="${Theme.text_color || "#000000"}">
+    ${public_repos} 📕
+    </text>
+    <text fill="${Theme.text_color || "#000000"}" font-size="18" x="147.7" y="233.5">
+    ${public_gists} 📑
+    </text>
+    <text fill="${Theme.text_color || "#000000"}" font-size="18" x="263.7" y="233.5">
+    ${orgLength} 🧑‍💻
+    </text>
+    <image href="${avatar_url}" x="398" class="profile_image" y="26" height="216" width="220" />
+</svg>`
+    )
+};
--- pages/api/github/user.ts
+++ pages/api/github/user.ts
@@ -1,13 +1,74 @@
+import Image from 'next/image';
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import UserProfile from '../../../components/userProfile'
+import themes from '../../../themes';
 
-type Data = {
-    name: string
+
+interface REPOTHEME {
+    title_color: string;
+    icon_color: string;
+    text_color: string;
+    bg_color: string;
+}
+// get a repo proprty 
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // get the repo name from the request
+    const User = req.query.user as string
+    const Themes = req.query.theme as string
+
+    // get the Theme for themes
+    const theme = Themes as keyof typeof themes;
+    const Theme = themes[theme] as REPOTHEME || themes.default as REPOTHEME;
+
+
+    // get the repo data from github
+    const data = await fetch(`https://api.github.com/users/${User}`)
+    const Org = await fetch(`https://api.github.com/users/${User}/orgs`)
+    const org = await Org.json()
+    // get the length of org
+    const orgLength = org.length
+    const repoData = await data.json()
+    const {
+        login,
+        avatar_url,
+        name,
+        bio,
+        location,
+        blog,
+        twitter_username,
+        public_repos,
+        public_gists,
+        followers,
+        following,
+        created_at,
+        email
+
+    } = repoData
+    // get the hmtl code 
+    const html = await UserProfile({
+        login,
+        avatar_url,
+        name,
+        bio,
+        location,
+        blog,
+        twitter_username,
+        public_repos,
+        public_gists,
+        followers,
+        following,
+        created_at,
+        email,
+        orgLength,
+        Theme
+    })
+
+    res.setHeader('Content-Type', 'image/svg+xml')
+    res.statusCode = 200;
+    return res.send(html)
 }
 
-export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Data>
-) {
-    res.status(200).json({ name: 'John Doe' })
-}
+
+// example endpoint ?
+// http://localhost:3000/api/github/user?user=Abbhiishek&theme=dracula
